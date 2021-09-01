d1bd2a828155e9fb83172a2c2dba0889c42744da
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-09-01 10:41:29 -0400

    Métodos agregados:  -menu  -ingresarOpcion  -ingresarEntrada  -iniciar

--- src/main/java/Taller01Sismos.java
+++ src/main/java/Taller01Sismos.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Taller01Sismos {
 
 
@@ -7,7 +9,11 @@ public class Taller01Sismos {
     }
 
     public static void menu() {
-
+        System.out.println("Elige una opción");
+        System.out.println("[1] Mostrar sismo de mayor magnitud");
+        System.out.println("[2] Contar sismos mayores o iguales a 5.0");
+        System.out.println("[3] Enviar un SMS por cada sismo mayor o igual a 7.0");
+        System.out.println("[4] Salir");
     }
 
     public static Double[][] crearArreglo() {
@@ -16,7 +22,6 @@ public class Taller01Sismos {
     }
 
     public static Double[][] llenarArreglo(Double[][] arreglo) {
-
         for (int i = 0; i < arreglo.length; i++) {
             for (int j = 0; j < arreglo[0].length; j++) {
                 arreglo[i][j] = Math.random() * 10;
@@ -27,7 +32,6 @@ public class Taller01Sismos {
     }
 
     public static double buscarMayorSismo(Double[][] arreglo) {
-
         double mayor = arreglo[0][0];
 
         for (int i = 0; i < arreglo.length; i++) {
@@ -42,7 +46,6 @@ public class Taller01Sismos {
     }
 
     public static double contarSismos(Double[][] arreglo) {
-
         double contador = 0;
 
         for (int i = 0; i < arreglo.length; i++) {
@@ -65,6 +68,61 @@ public class Taller01Sismos {
                 }
             }
         }
+    }
+
+    public static String elegirOpcion() {
+
+        System.out.println("Ingrese una opción");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static Integer ingresarEntrada() {
+
+        try {
+            return Integer.parseInt(elegirOpcion());
+        } catch (NumberFormatException e) {
+            System.err.println("ERROR: vuelva a ingresar la opción");
+            return ingresarEntrada();
+        }
+    }
+
+    public static void iniciar() {
+
+        var eleccion = 0;
+        do {
+            menu();
+            eleccion = ingresarEntrada();
+
+            switch (eleccion) {
+
+                case 1: {
+                    var arreglo = llenarArreglo(crearArreglo());
+
+                    System.out.println("El sismo de mayor magnitud es: " + buscarMayorSismo(arreglo));
+                }
+                break;
+
+                case 2: {
+                    var arreglo = llenarArreglo(crearArreglo());
+                    System.out.println("El número de sismos mayores o iguales a 5.0 es " + contarSismos(arreglo));
+                }
+                break;
+                case 3: {
+                    var arreglo = llenarArreglo(crearArreglo());
+                    enviarSMS(arreglo);
+                    break;
+                }
+                case 4:
+                    System.out.println("¡Hasta pronto!");
+                    break;
+
+                default:
+                    System.err.println("La opción ingresada no es válida");
+                    break;
+            }
+        } while (eleccion != 4);
+
+
     }
 
 }
