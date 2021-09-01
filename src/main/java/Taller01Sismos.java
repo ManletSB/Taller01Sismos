4bbebfa699ee99c3509cd8a7bebfb093f4d0e4f5
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-09-01 10:45:08 -0400

    Llenado el main. Finalizado.

--- src/main/java/Taller01Sismos.java
+++ src/main/java/Taller01Sismos.java
@@ -5,7 +5,8 @@ public class Taller01Sismos {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Bienvenido a Sismografía");
+        iniciar();
     }
 
     public static void menu() {
